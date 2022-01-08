222797e3296c6a8805f79843a097289874d1132d
Leon Si <contact@leonzalion.com>
2022-01-07 23:57:33 -0500

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-	base: '/timeblocker/',
+	base: '/timeblocker-ics/',
 	resolve: {
 		alias: {
 			'~': path.resolve(new URL('.', import.meta.url).pathname, './src'),
